1286132d7808e596f22294ddb2e8eb513747d280
Yordi Ogando <yordiogando@gmail.com>
2019-05-22 12:48:50 -0400

    Segundo Intento

---
+++ 22-05-2019(Sorting Balls).cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Sorting_Balls_2
+{
+    class Program
+    {
+        class Rack
+        {
+            int[] balls = new int[1];
+            int Size = 0;
+
+            public void Add(int value)
+            {
+                if (Size == 0)
+                {
+                    balls[0] = value;
+                    Size++;
+                }
+
+                else Sort(value);
+            }
+
+            public void Sort(int value)
+            {
+                int j = 0;
+                int[] temp = new int[balls.Length + 1];
+                bool ball_Added = false;
+
+                for (int i = 0; j < temp.Length; i++)
+                {
+                    if (i == balls.Length) temp[j] = value;
+
+                    else if (balls[i] < value ||ball_Added)
+                    {
+                        temp[j] = balls[i];
+                    }
+
+                    else
+                    {
+                        temp[j] = value; ;
+                        i--;                  
+                        ball_Added = true;
+                    }
+                    j++;
+                }
+
+                balls = temp;
+            }
+
+            public string ShowBalls()
+            {
+                string output = "";
+                foreach(int num in balls)
+                {
+                    output += Convert.ToString(num) + " - ";
+                }
+                return output.Remove(output.Length-3);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Rack rack = new Rack();
+            Console.WriteLine("Insert a number: ");
+            while (true)
+            {
+                int value = Convert.ToInt32(Console.ReadLine());
+                rack.Add(value);
+                Console.WriteLine("\n" + rack.ShowBalls() + "\n");
+
+            }
+
+            
+        }
+    }
+}
---
+++ 22-05-2019(Sorting Characters).cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Sorting_Characters_2
+{
+    class Program
+    {
+        public static string Decoder (string sentence)
+        {
+            int[] freq = new int[26];
+            string output = "";
+            char character = '1';
+
+            sentence = sentence.ToLower();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+               if (sentence[i] >=97 && sentence[i] <=122) freq[sentence[i] - 97]++;
+            }
+
+            for (int i = 0; i < freq.Length; i++)
+            {
+                for (int j = freq[i]; j >0; j--)
+                {
+                    character = Convert.ToChar(i + 97);
+                    output += character;
+                }
+            }
+
+            return output;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            string sentence = "When not studying nuclear physics, Bambi likes to play beach volleyball.";
+
+            Console.WriteLine(Decoder(sentence));
+        }
+    }
+}
