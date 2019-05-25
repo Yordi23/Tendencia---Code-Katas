5f7f2c4c24b2ed3243f8f4bbbda9d75cfb86479c
Yordi Ogando <yordiogando@gmail.com>
2019-05-24 22:30:59 -0400

    Tercer intento

---
+++ 24-05-2019(Sorting Balls).cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Sorting_Balls_3
+{
+
+    class  Rack
+    {
+        private int Size = 0;
+        private int[] balls = new int[1];
+
+        public void Add(int value)
+        {
+
+            if (Size == 0)
+            {
+                balls[0] = value;
+                Size++;
+            }
+
+            else Sort(value);
+
+        }
+
+        private void Sort(int value)
+        {
+            int j = 0;
+            int[] temp = new int[balls.Length + 1];
+            bool ball_Added = false;
+
+            for (int i=0; j<temp.Length; i++)
+            {
+                if (i == balls.Length) temp[j] = value;
+
+                else if (ball_Added || balls[i] < value) temp[j] = balls[i];
+
+                else
+                {
+                    ball_Added = true;
+                    temp[j] = value;
+                    i--;
+                }
+                j++;
+
+            }
+            balls = temp;
+
+        }
+
+        public string ShowBalls ()
+        {
+            string output = "";
+
+            foreach (int num in balls)
+                output += Convert.ToString(num) + " - ";
+
+            return output.Remove(output.Length-2);
+        }
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rack rack = new Rack();
+
+            int value;
+            while (true)
+            {
+                value = Convert.ToInt32(Console.ReadLine());
+                rack.Add(value);
+
+                Console.WriteLine("\n" + rack.ShowBalls() + "\n");
+
+            }
+
+
+        }
+    }
+}
---
+++ 24-05-2019(Sorting Characters).cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Sorting_Characters_3
+{
+    class Program
+    {
+        public static void Decoder(string sentence)
+        {
+            int[] freq = new int[26];
+            char character = '0';
+            string output = "";
+            sentence = sentence.ToLower();
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] >= 97 && sentence[i] <= 122) freq[sentence[i]-97]++;
+            }
+
+            for (int i = 0; i < freq.Length; i++)
+            {
+                for (int j = freq[i]; j > 0; j--)
+                {
+                    character = Convert.ToChar(i + 97);
+                    output += character;
+                }
+            }
+
+
+            Console.WriteLine(output);
+        }
+
+
+        static void Main(string[] args)
+        {
+            string sentence = "When not studying nuclear physics, Bambi likes to play beach volleyball.";
+            Decoder(sentence);
+            //aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy
+        }
+    }
+}
