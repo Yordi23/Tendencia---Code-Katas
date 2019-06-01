32ecf895268dab434f6c366b792f03ad41ce7d59
Yordi Ogando <yordiogando@gmail.com>
2019-06-01 18:34:10 -0400

    First

---
+++ 01-06-2019/StringCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public string Add(string expression)
+        {
+            Double sum = 0;
+            string[] numbers = expression.Split(',');
+
+            foreach (string num in numbers)
+            {
+                if (num == "") continue;
+                sum += Convert.ToDouble(num); 
+            }
+
+            return Convert.ToString(sum); 
+        } 
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 01-06-2019/UnitTests/Tests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using StringCalculator;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        Calculator calculator = new Calculator();
+
+        [Test]
+        public void EmptyString()
+        {
+            Assert.AreEqual("0", calculator.Add(""));
+        }
+
+        [Test]
+        public void OneNumber()
+        {
+            Assert.AreEqual("5", calculator.Add("5"));
+        }
+
+        [Test]
+        public void TwoNumbers()
+        {
+            Assert.AreEqual("9", calculator.Add("2,7"));
+        }
+
+        [Test]
+        public void ThreeNumbers()
+        {
+            Assert.AreEqual("15", calculator.Add("5,9,1"));
+        }
+
+        [Test]
+        public void FourDecimalNumbers()
+        {
+            Assert.AreEqual("4.4", calculator.Add("1.1,0.5,0.5,2.3"));
+        }
+    }
+}
