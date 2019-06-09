823fccb89f23e8b51b4eacd6c5d5c57a3e21b3e9
Yordi23 <39386472+Yordi23@users.noreply.github.com>
2019-06-08 23:49:10 -0400

    Primer Intento

---
+++ 08-06-2019/NumberInWords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberInWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Translator
+    {
+        string[] numbers = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "a-hundred", "a-thousand", "a-million" };
+
+        public string translate(int num)
+        {
+            if (num >= 0 && num <= 20)
+                return numbers[num];
+
+            else
+            {
+                if ()
+            }
+
+            
+        }
+    }
+}
---
+++ 08-06-2019/UnitTests/Tests.cs
@@ -0,0 +1,45 @@
+﻿using NumberInWords;
+using NUnit.Framework;
+using System;
+
+
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Zero()
+        {
+            Translator translator = new Translator();
+
+            Assert.AreEqual("Zero", translator.translate(0));
+        }
+
+        [Test]
+        public void One()
+        {
+            Translator translator = new Translator();
+
+            Assert.AreEqual("One", translator.translate(1));
+        }
+
+        [Test]
+        public void Ten()
+        {
+            Translator translator = new Translator();
+
+            Assert.AreEqual("Ten", translator.translate(10));
+        }
+
+        [Test]
+        public void Twenty_one()
+        {
+            Translator translator = new Translator();
+
+            Assert.AreEqual("Twenty One", translator.translate(21));
+        }
+
+    }
+}
