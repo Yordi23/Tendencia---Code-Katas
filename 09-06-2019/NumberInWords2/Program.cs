f3f4f8c69d85afdda05c310bc73378a97fc508e2
Yordi23 <39386472+Yordi23@users.noreply.github.com>
2019-06-09 12:10:06 -0400

    Segundo Intento

---
+++ 09-06-2019/NumberInWords2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NumberInWords2
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Translator
+    {
+        string[] numbers = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Hundred", "Thousand", "Million" };
+        public string Translate(string num)
+        {
+            int IntNum = Convert.ToInt32(num);
+            string output = "";
+
+            if (IntNum >= 0 && IntNum <= 20)
+                return numbers[IntNum];
+
+            switch (num.Length)
+            {
+                case 2:
+                    output = TwoDigits(num) + " " + Translate(num.Substring(1));
+                    break;
+
+                case 3:
+                    output = Translate(num.Substring(0, 1)) + " " + numbers[28] + " " + TwoDigits(num.Substring(1,2));
+                    break;
+
+            }
+            return output;
+        }
+
+        private string TwoDigits(string num)
+        {
+            return numbers[Convert.ToInt32(num.Substring(0, 1)) + 18];
+        }
+
+    }
+}
---
+++ 09-06-2019/UnitTests/Tests.cs
@@ -0,0 +1,59 @@
+﻿using NumberInWords2;
+using NUnit.Framework;
+
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Zero ()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Zero", translator.Translate("0"));
+        }
+
+        [Test]
+        public void One()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("One", translator.Translate("1"));
+        }
+
+        [Test]
+        public void Twenty_One()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Twenty One", translator.Translate("21"));
+        }
+
+        [Test]
+        public void Fifty_Five()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Fifty Five", translator.Translate("55"));
+        }
+
+        [Test]
+        public void Ninety_Nine()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Ninety Nine", translator.Translate("99"));
+        }
+
+        [Test]
+        public void One_Hundred()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("One Hundred", translator.Translate("100"));
+        }
+
+        [Test]
+        public void Two_Hundred_Forty_Eight()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Two Hundred Forty Eight", translator.Translate("248"));
+        }
+    }
+}
