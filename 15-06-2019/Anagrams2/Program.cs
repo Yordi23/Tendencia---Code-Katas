184cabeb009352742c65191fe8da6fdc668af643
Yordi23 <39386472+Yordi23@users.noreply.github.com>
2019-06-15 20:41:21 -0400

    Second

---
+++ 15-06-2019/Anagrams2/Program.cs
@@ -0,0 +1,89 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace Anagrams2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class AnagramFinder
+    {
+        List<char>[] sortedChars;
+
+        public string[] Find(string[] words)
+        {
+            sortedChars = ListSorter(words);
+            int counter = 0;
+            string[] output = new string[3];
+            bool isAnagram = false;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == null) continue;
+
+                for (int j = i+1; j < words.Length; j++)
+                {
+                    if (words[j] == null) continue;
+
+                    if (words[i].Length == words[j].Length)
+                    {
+                        if (ListComparer(sortedChars[i], sortedChars[j]))
+                        {
+                            if (isAnagram == false) output[counter] = words[i];
+
+                            output[counter] += " * " + words[j];
+                            words[j] = null;
+                            isAnagram = true;
+                        }
+                    }
+                }
+                counter++;
+                isAnagram = false;
+            }
+           
+            return output;
+        }
+
+        List<char>[] ListSorter(string[] words)
+        {
+            List<char>[] output = new List<char>[words.Length];
+            
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var tempList = new List<char>();
+                foreach (var item in words[i])
+                {
+                    tempList.Add(item);
+                }
+
+                tempList.Sort();
+                output[i] = tempList;
+            }
+            return output;
+
+        }
+
+        bool ListComparer(List<char> List1, List<char> List2)
+        {
+            bool equal = true;
+            foreach (var item in List1)
+            {
+                if (!List2.Contains(item))
+                {
+                    equal = false;
+                    break;
+                }
+            }
+
+            return equal;
+        }
+    } 
+}
+
+
---
+++ 15-06-2019/UnitTests/Tests.cs
@@ -0,0 +1,54 @@
+﻿using Anagrams2;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+
+    public class Tests
+    {
+        AnagramFinder finder = new AnagramFinder();
+
+        [Test]
+        public void OneWord()
+        {
+  
+            string[] words = { "hola" };
+            string[] output = finder.Find(words);
+
+            Assert.AreEqual(null, output[0]);
+
+        }
+
+        [Test]
+        public void TwoNotAnagramsWords()
+        {
+            string[] words = { "hola","mundo" };
+            string[] output = finder.Find(words);
+
+            Assert.AreEqual(null, output[0]);
+            Assert.AreEqual(null, output[1]);
+        }
+
+        [Test]
+        public void TwoAnagramWords()
+        {
+            string[] words = { "hola", "laho" };
+            string[] output = finder.Find(words);
+
+            Assert.AreEqual("hola * laho", output[0]);
+        }
+
+        [Test]
+        public void SixAnagramWords_FourNotAnagrams()
+        {
+            string[] words = { "hola", "laho","libro", "ohla","broli", "cabeza","rolib", "pie", "persona","teclado" };
+            string[] output = finder.Find(words);
+
+            Assert.AreEqual("hola * laho * ohla", output[0]);
+            Assert.AreEqual("libro * broli * rolib", output[1]);
+            Assert.AreEqual(null, output[2]);
+        }
+
+    }
+}
