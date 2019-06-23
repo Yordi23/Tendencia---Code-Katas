73bb77cab3d57778d81af21d9c57c835ff22de77
Yordi23 <39386472+Yordi23@users.noreply.github.com>
2019-06-22 20:00:58 -0400

    First

---
+++ 22-06-2019/RomanNumerals/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace RomanNumerals
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {}
+    }
+
+    public class Converter
+    {
+        string[] RomanNumerals = { "I","IV", "V","IX", "X","XL", "L", "C", "D", "M" };
+        int[] Numbers = {1,4,5,9,10,40,50,100,500,1000 };
+
+        public string Convert(int num)
+        {
+            string output = "";
+
+            for (int i = RomanNumerals.Length-1; i >= 0; i--)
+            {
+                if (Numbers[i] <= num)
+                {
+                    output += RomanNumerals[i];
+                    num -= Numbers[i];
+                    i++;
+                }
+            }
+
+            
+
+            return output;
+        }
+    }
+}
---
+++ 22-06-2019/UnitTests/Tests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+
+namespace UnitTests
+{
+    [TestFixture]
+
+    public class Class1
+    {
+        Converter converter = new Converter();
+        [Test]
+        public void One_ShouldReturn_I()
+        {
+            Assert.AreEqual("I", converter.Convert(1));
+        }
+
+        [Test]
+        public void Five_ShouldReturn_V()
+        {           
+            Assert.AreEqual("V", converter.Convert(5));
+        }
+
+        [Test]
+        public void VariousNumbers()
+        {
+            Assert.AreEqual("X", converter.Convert(10));
+            Assert.AreEqual("L", converter.Convert(50));
+            Assert.AreEqual("C", converter.Convert(100));
+        }
+
+        [Test]
+        public void Three_ShouldReturn_III()
+        {
+            Assert.AreEqual("III", converter.Convert(3));
+        }
+
+        [Test]
+        public void Four_ShouldReturn_IV()
+        {
+            Assert.AreEqual("IV", converter.Convert(4));
+        }
+
+
+        [Test]
+        public void VariousNumbersII()
+        {
+            Assert.AreEqual("IX", converter.Convert(9));
+            Assert.AreEqual("XLIX", converter.Convert(49));
+            Assert.AreEqual("XXVII", converter.Convert(27));
+        }
+    }
+}
