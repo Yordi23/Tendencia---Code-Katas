70a400d6c670ba4fdc136668995953bb1df23a4b
Yordi23 <39386472+Yordi23@users.noreply.github.com>
2019-06-28 00:19:58 -0400

    Word Wrap - First

---
+++ 28-06-2019/UnitTests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using WordWrap;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public static void Test1()
+        {
+            Wrapper wrapper = new Wrapper();
+            Assert.AreEqual("hola\nJuan", WordWrap.Wrapper.Wrap("hola Juan", 4));
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            Wrapper wrapper = new Wrapper();
+            Assert.AreEqual("si no\nhola", WordWrap.Wrapper.Wrap("si no hola", 4));
+        }
+
+    }
+}
---
+++ 28-06-2019/WordWrap/Wrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WordWrap
+{
+    public class Wrapper
+    {
+        public static string Wrap(string input, int columnLength)
+        {
+            int count = 0;
+            string output = "";
+            string tempWord = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                tempWord += input[i];
+
+                if (input[i] == ' ' || i == input.Length-1) {
+
+                    if (count >= columnLength)
+                    {
+                        if (tempWord[0] == ' ') tempWord = tempWord.Substring(1);
+                        output += "\n" + tempWord;
+                    }
+                    else output += tempWord;
+
+                    count = 0;
+                    tempWord = "";
+                }
+
+                count++;
+
+            }
+
+
+            return output;
+        }
+    }
+
+
+}
