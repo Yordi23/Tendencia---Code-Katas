c94b6516fa4934660eaca8ba2b94621d665c12ce
Yordi23 <39386472+Yordi23@users.noreply.github.com>
2019-06-30 13:54:41 -0400

    Word Wrap Segundo Intento

---
+++ 30-06-2019/UnitTests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("hola\nPedro", WordWrap2.Wrapper.Wrap("hola Pedro",4));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("sol sal\nEdificio", WordWrap2.Wrapper.Wrap("sol sal Edificio", 6));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual("si\na a\nni\no", WordWrap2.Wrapper.Wrap("si a a ni o", 2));
+        }
+    }
+}
---
+++ 30-06-2019/WordWrap2/Wrapper.cs
@@ -0,0 +1,46 @@
+﻿
+namespace WordWrap2
+{
+    public class Wrapper
+    {
+        public static string Wrap(string sentence, int columnLenght)
+        {
+            string word = "", output = "";
+            int count = 0;
+
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                word += sentence[i];
+
+                if (sentence[i] == ' ' || i ==  sentence.Length)
+                {
+                    if (count >= columnLenght)
+                    {
+                        output +=  word.Substring(0,word.Length-1) + "\n" ;
+                        count = 0;
+                        
+                    }
+                    else output += word;
+
+                    word = "";
+                }
+
+                count++;
+
+            }
+
+            
+            return output;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
